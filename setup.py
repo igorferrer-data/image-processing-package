14b7732067c0114273c72a2bb497d1741b9f845b
Igor Ferrer <igori83@gmail.com>
2024-10-14 00:33:08 -0300

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+name - "image_processing",
+version = "0.1",
+author = "Igor_Ferrer",
+author_email = "igori83@gmail.com",
+description= "pacote de processamento de imagens",
+long_description = page_description,
+long_description_content_type = "text/markdown",
+url = "https://github.com/igorferrer-data/image-processing-package"
+packages = find_packages(),
+python_requires ='>=3,8',
+)
