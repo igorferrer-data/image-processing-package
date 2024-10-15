d5b94e13b0d0678a359aeebfc7915a3b2b8faab3
Igor Ferrer <igori83@gmail.com>
2024-10-15 01:03:12 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+from setuptools import setup, find_packages
+
 setup(
-name = "image_processing",
-version = "0.1",
-author = "Igor_Ferrer",
-author_email = "igori83@gmail.com",
-description= "pacote de processamento de imagens",
-long_description = page_description,
-long_description_content_type = "text/markdown",
-url = "https://github.com/igorferrer-data/image-processing-package"
-packages = find_packages(),
-python_requires ='>=3,8',
+    name = "image_processing",
+    version = "0.0.1",
+    author = "Igor_Ferrer",
+    author_email = "igori83@gmail.com",
+    description= "pacote de processamento de imagens",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/igorferrer-data/image-processing-package",
+    packages = find_packages(),
+    install_requires=[
+    'requests',
+    'jupyterlab>=4.0.0,<5.0.0'
+    ],
+
+    python_requires='>=3.8',
 )
